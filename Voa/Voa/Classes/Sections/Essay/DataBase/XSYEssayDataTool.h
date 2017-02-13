355b7fffe97abc23f5314b5f4274680ec22fddd7
xsy <1006852969@qq.com>
2017-02-13 11:47:16 +0800

    文章界面fmdb完成

---
+++ Voa/Voa/Classes/Sections/Essay/DataBase/XSYEssayDataTool.h
@@ -0,0 +1,13 @@
+//
+//  XSYEssayDataTool.h
+//  Voa
+//
+//  Created by xin on 2017/2/13.
+//  Copyright © 2017年 DogeEggEgg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XSYEssayDataTool : NSObject
+
+@end
---
+++ Voa/Voa/Classes/Sections/Essay/DataBase/XSYEssayDataTool.m
@@ -0,0 +1,13 @@
+//
+//  XSYEssayDataTool.m
+//  Voa
+//
+//  Created by xin on 2017/2/13.
+//  Copyright © 2017年 DogeEggEgg. All rights reserved.
+//
+
+#import "XSYEssayDataTool.h"
+
+@implementation XSYEssayDataTool
+
+@end
